28645ffeb029c4e609f93c5a27680afcec3e519d
harika penukonda <penukondaharika@gmail.com>
2020-04-09 04:14:02 -0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -83,10 +83,12 @@ function rpsGame(yourChoice){
         var botDiv = document.createElement('div');
         var messageDiv = document.createElement('div');
 
-        humanDiv.innerHTML = "<img src ='" + imageDatabase[humanImageChoice]+ "' height=150 width=150 style:'box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
-        botDiv.innerHTML = "<img src ='" + imageDatabase[computerImageChoice]+ "' height=150 width=150 style:'box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
+       humanDiv.innerHTML   = "<img src='" + imageDatabase[humanImageChoice]+ "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+       botDiv.innerHTML   = "<img src='" + imageDatabase[computerImageChoice]+ "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
+       messageDiv.innerHTML = "<h1 style='color : " + finalMessage['color'] +"; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
 
         document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+        document.getElementById('flex-box-rps-div').appendChild(messageDiv);
         document.getElementById('flex-box-rps-div').appendChild(botDiv);
     }
 
